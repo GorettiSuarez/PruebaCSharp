b30c1074e15210534a8d96110d695ed8ce74b3df
Goretti Suarez <goretti__sr.95@hotmail.es>
2018-03-12 11:46:30 +0000

    Object Client created

---
+++ PruebaCSharp/Models/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PruebaCSharp.Models
+{
+    public class Client
+    {
+
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string lastName { get; set; }
+
+        public string dni { get; set; }
+
+        //TO-DO: check type of object for photo
+        public string photo { get; set; }
+
+        //TO-DO: check type of object for product list
+        public string productList { get; set; }
+
+
+    }
+}
